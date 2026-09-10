8594050a3a41e1b4ef0955bcf4a7a99d2d6405dd
tobeprecisetech <tobeprecisetech@gmail.com>
2026-09-10 16:57:43 +0400

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -15,6 +15,6 @@ app.get("/api/health", (req, res) => {
   });
 });
 
-app.listen(PORT, () => {
-  console.log(`Backend running on http://localhost:${PORT}`);
+app.listen(PORT, "0.0.0.0", () => {
+  console.log(`Backend running on port ${PORT}`);
 });
